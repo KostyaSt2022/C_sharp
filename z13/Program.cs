d74da4f94e67a1de17c0c0fa9e204b194b17668f
Kostya <kotekostya@gmail.com>
2022-10-17 19:00:03 +0400

    Сделаны задачи 10, 13, 15, 9(доп)

---
+++ z13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число:");
+int numb = int.Parse(Console.ReadLine()!);
+string lenghtOfNumb = numb.ToString();
+
+if (lenghtOfNumb.Length > 2)
+{
+    Console.WriteLine($"Третья цифра числа {numb}: {lenghtOfNumb[2]}");
+}
+else
+{
+    System.Console.WriteLine($"Третьей цифры в числе {numb} нет!");
+}
---
+++ z15/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели:");
+int numb = int.Parse(Console.ReadLine()!);
+if (numb >= 1 && numb <= 7)
+{
+    if (numb == 6 | numb == 7)
+    Console.WriteLine("Сегодня выходной");
+    else 
+    Console.WriteLine("Сегодня рабочий день");
+}
+else 
+{
+    Console.WriteLine("Такого дня недели не существует!");
+}
